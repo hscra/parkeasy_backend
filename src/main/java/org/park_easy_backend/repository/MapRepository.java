e9474a9c397ac55fc8ea309903f543f595061d81
OskarCurzytek <oskarcurzytek@gmail.com>
2024-12-01 01:24:41 +0100

    Fixes to map service

--- src/main/java/org/park_easy_backend/repository/MapRepository.java
+++ src/main/java/org/park_easy_backend/repository/MapRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MapRepository extends JpaRepository<LocationEntity, Long> {
-    Optional<LocationEntity> findByName(String name);
+    Optional<LocationEntity> findLocationEntityByCity(String name);
 }
--- src/main/java/org/park_easy_backend/service/MapService.java
+++ src/main/java/org/park_easy_backend/service/MapService.java
@@ -1,26 +1,26 @@
 package org.park_easy_backend.service;
 
+import lombok.RequiredArgsConstructor;
 import org.park_easy_backend.dto.LocationDTO;
-import org.park_easy_backend.dto.MemberDTO;
 import org.park_easy_backend.entity.LocationEntity;
-import org.park_easy_backend.entity.MemberEntity;
 import org.park_easy_backend.repository.MapRepository;
-import org.park_easy_backend.repository.MemberRepository;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import javax.xml.stream.Location;
 import java.util.Optional;
 
+@Service
+@RequiredArgsConstructor
 public class MapService {
     private final MapRepository mapRepository;
 
     public void save(LocationDTO locationDTO){
         LocationEntity locationEntity = LocationEntity.toLocationEntity(locationDTO);
-        MapRepository.save(locationEntity);
+        mapRepository.save(locationEntity);
     }
 
     public LocationDTO find(LocationDTO locationDTO){
-        Optional<LocationEntity> byLocationName = MapRepository.findByName(locationDTO.getCity());
+        Optional<LocationEntity> byLocationName = mapRepository.findLocationEntityByCity(locationDTO.getCity());
         return byLocationName.map(LocationDTO::toLocationDTO).orElse(null);
     }
 }
