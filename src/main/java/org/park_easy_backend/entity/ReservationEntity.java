db02df32e6009b421ff1b4ca8b51171fe64d8ab9
OskarCurzytek <oskarcurzytek@gmail.com>
2025-02-04 22:35:52 +0100

    added hours to reservations

--- src/main/java/org/park_easy_backend/entity/ReservationEntity.java
+++ src/main/java/org/park_easy_backend/entity/ReservationEntity.java
@@ -26,11 +26,19 @@ public class ReservationEntity {
     @Column(name = "parking_space_id", nullable = false)
     private Long parkingSpaceId;
 
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalDateTime endTime;
+
     public static ReservationEntity toReservationEntity(ReservationDTO DTO){
         ReservationEntity entity = new ReservationEntity();
         entity.setReservationId(DTO.getReservationId());
         entity.setUserId(DTO.getUserId());
         entity.setParkingSpaceId(DTO.getParkingSpaceId());
+        entity.setStartTime(DTO.getStartTime());
+        entity.setEndTime(DTO.getEndTime());
 
         return entity;
     }
